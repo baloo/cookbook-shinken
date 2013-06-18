abd1513a0e24985880d98dd1580c5cd620a94e29
Martha Greenberg <marthag@techfinancials.com>
2013-06-18 17:36:11 +0300

    setup pnp4nagios

---
+++ recipes/pnp.rb
@@ -0,0 +1,15 @@
+package "pnp4nagios"
+package "php-fpm"
+
+service "php-fpm" do
+  action [:enable, :start]
+end
+
+service "npcd" do
+  action [:enable, :start]
+end
+
+template "#{node['apache']['dir']}/conf.d/pnp4nagios.conf" do
+  mode 0644
+  notifies :reload, "service[apache2]", :delayed
+end
